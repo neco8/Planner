95a2c45624c1c8107dadd8b855d0636a87681384
neco8 <shogibeshow@gmail.com>
2022-01-14 10:41:59 +0900

    fix todo structure

--- haskell-abyss/src/Todo.hs
+++ haskell-abyss/src/Todo.hs
@@ -1,3 +1,13 @@
 module Todo (Todo (..)) where
 
-data Todo s = Todo Bool s [Todo s] deriving (Eq, Ord, Show)
+import Data.String (IsString)
+import PPrint (PPrint, pprint)
+
+data Todo s = Todo Bool s deriving (Eq, Ord, Show)
+
+instance PPrint s => PPrint (Todo s) where
+  pprint =
+    showDoneWith $ \b -> "- [" <> (if b then "x" else " ") <> "] "
+    where
+      showDoneWith f (Todo b s) =
+        f b <> pprint s
