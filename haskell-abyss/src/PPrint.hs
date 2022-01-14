b65e92fb0ffb851685fa2c5028b2cce8986f2ce3
neco8 <shogibeshow@gmail.com>
2022-01-14 10:42:09 +0900

    add tree pprint

--- haskell-abyss/src/PPrint.hs
+++ haskell-abyss/src/PPrint.hs
@@ -1,4 +1,11 @@
 module PPrint where
 
+import Data.Tree
+import Data.List (intercalate)
+
 class PPrint a where
   pprint :: a -> String
+
+instance PPrint a => PPrint (Tree a) where
+  pprint tree =
+    intercalate "\n" $ pprint (rootLabel tree) : (("  " <>) <$> concatMap (lines . pprint) (subForest tree))
