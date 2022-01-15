9152e875c40796176555127047507012386ab377
neco8 <shogibeshow@gmail.com>
2022-01-15 16:05:07 +0000

    add float pprint

--- haskell-abyss/src/PPrint.hs
+++ haskell-abyss/src/PPrint.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module PPrint where
 
-import           Data.List (intercalate)
-import qualified Data.Text as T (Text, intercalate, lines)
-import           Data.Tree
+import           Control.Arrow ((&&&))
+import           Data.List     (intercalate)
+import           Data.Ratio    (denominator, numerator)
+import           Data.String   (IsString (fromString))
+import qualified Data.Text     as T (Text, intercalate, lines)
+import           Data.Tree     (Tree (rootLabel, subForest))
 
 class PPrint a where
   pprint :: a -> T.Text
@@ -11,3 +14,9 @@ class PPrint a where
 instance PPrint a => PPrint (Tree a) where
   pprint tree =
     T.intercalate "\n" (pprint (rootLabel tree) : (("  " <>) <$> concatMap (T.lines . pprint) (subForest tree)))
+
+instance PPrint Float where
+  pprint f =
+    case (numerator &&& denominator) $ toRational f of
+      (a, 1) -> fromString $ show a
+      _      -> fromString $ show f
