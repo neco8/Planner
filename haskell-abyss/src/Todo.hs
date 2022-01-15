3f1e95f86baee314c72ee84ae46fd56015f9c8cf
neco8 <shogibeshow@gmail.com>
2022-01-15 12:13:14 +0000

    add todo parser

--- haskell-abyss/src/Todo.hs
+++ haskell-abyss/src/Todo.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE OverloadedLists   #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Todo (Todo (..)) where
-
-import           PPrint (PPrint, pprint)
+module Todo (Todo (..), todoParser) where
+import           Data.Functor         ((<$))
+import           Data.Vector          (Vector)
+import           PPrint               (PPrint, pprint)
+import           Parser               (Parser)
+import           Text.Megaparsec      (choice, try, (<|>))
+import           Text.Megaparsec.Char (char, string)
 
 data Todo s = Todo Bool s deriving (Eq, Ord, Show)
 
@@ -11,3 +16,23 @@ instance PPrint s => PPrint (Todo s) where
     where
       showDoneWith f (Todo b s) =
         f b <> pprint s
+
+-- parser
+
+todoParser_ :: Vector (Parser Bool) -> Parser s -> Parser (Todo s)
+todoParser_ pdones ps = do
+  b <- try (choice pdones) <|> pure False
+  Todo b <$> ps
+
+doneMDParser :: Parser Bool
+doneMDParser = do
+  string "- ["
+  b <- (True <$ char 'x') <|> (False <$ char ' ')
+  string "] "
+  pure b
+
+doneVsCodeParser :: Parser Bool
+doneVsCodeParser = try (True <$ string "✔ ") <|> (False <$ string "☐ ")
+
+todoParser :: Parser s -> Parser (Todo s)
+todoParser = todoParser_ [doneMDParser, doneVsCodeParser]
