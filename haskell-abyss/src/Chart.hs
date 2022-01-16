2f38760cd5b53b0ef7f856ad67035ab345e202e8
neco8 <shogibeshow@gmail.com>
2022-01-16 15:34:02 +0000

    fix chart

--- haskell-abyss/src/Chart.hs
+++ haskell-abyss/src/Chart.hs
@@ -6,10 +6,19 @@ import           ActionPriorityMatrix                      (ActionPriorityMatrix
                                                             runImpact)
 import           Data.Vector                               (Vector, toList)
 import           Graphics.Rendering.Chart.Backend.Diagrams (toFile)
-import           Graphics.Rendering.Chart.Easy             (Default (def), navy,
+import           Graphics.Rendering.Chart.Easy             (Default (def),
+                                                            axis_grid,
+                                                            axis_labels,
+                                                            axis_ticks,
+                                                            axis_viewport,
+                                                            laxis_override,
+                                                            layout_x_axis,
+                                                            layout_y_axis, navy,
                                                             opaque, plot,
+                                                            plot_points_title,
                                                             points, setColors,
-                                                            to, (^.))
+                                                            to, vmap, (.=),
+                                                            (.~), (^.))
 
 values :: Monad m => m (ActionPriorityMatrix qwa) -> m (Float, Float)
 values apms = do
@@ -21,4 +30,17 @@ values apms = do
 getChart :: FilePath -> [ActionPriorityMatrix qwa] -> IO ()
 getChart path d = toFile def path $ do
   setColors [ opaque navy ]
+  layout_x_axis . laxis_override .=
+    ((axis_viewport .~ vmap (0, 10)) .
+      (axis_grid .~ [0..10]) .
+      (axis_ticks .~ ((\n -> (n, if n `elem` bigLabels then 4 else 2)) <$> [0, 0.2..10])) .
+      (axis_labels .~ [(\n -> (n, show n)) <$> [0, 2..10]])
+    )
+  layout_y_axis . laxis_override .=
+    ((axis_viewport .~ vmap (0, 10)) .
+      (axis_grid .~ [0..10]) .
+      (axis_ticks .~ ((\n -> (n, if n `elem` bigLabels then 4 else 2)) <$> [0, 0.2..10]))) .
+      (axis_labels .~ [(\n -> (n, show n)) <$> [0, 2..10]])
   plot $ points "Action Priority Matrix" $ values d
+  where
+    bigLabels = [0.0, 2.0..10.0]
