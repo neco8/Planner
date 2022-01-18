a589a2969fee6e5a99a23cbf32d1a629117770e9
neco8 <shogibeshow@gmail.com>
2022-01-18 23:47:05 +0900

    refactor compile program

--- haskell-abyss/app/Main.hs
+++ haskell-abyss/app/Main.hs
@@ -1,10 +1,15 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
+
 import           ActionPriorityMatrix   (ActionPriorityMatrix,
                                          MDActionPriorityMatrix (..), apmParser,
                                          qwas)
 import           Chart                  (getChart)
-import           Control.Monad.IO.Class (liftIO)
+import           Control.Applicative    ((<|>))
+import           Control.Monad.IO.Class (MonadIO, liftIO)
+import           Data.Coerce            (coerce)
 import           Data.Foldable          (all)
 import           Data.Functor           (void)
 import qualified Data.List              as L (sort)
@@ -15,7 +20,7 @@ import           Data.Vector            (Vector, fromList, partition, toList)
 import           ForWork                (ForWorkActionPriorityMatrix (..),
                                          changeFilePathForWork)
 import           Lens.Micro             (mapped, sets, to, (%~), (^.))
-import           Options.Declarative    (Cmd, Flag, Option (get), run)
+import           Options.Declarative    (Arg, Cmd, Flag, Option (get), run)
 import           PPrint                 (PPrint (pprint))
 import           Parser                 (treeParser)
 import           QuickWinAnalysis       (QuickWinAnalysis, qwaParser)
@@ -30,30 +35,53 @@ main = run "Planner" (Just "0.0.0") compile
 
 -- TODO: todo編集用のtui。設定ファイルがあって、それには入力用・保存用のファイルが書いてある。コマンドでも指定可能
 
-compile :: Flag "i" '["input"] "FILE_PATH" "input file path" (Maybe FilePath)
-  -> Flag "o" '["output"] "FILE_PATH" "output file path" (Maybe FilePath)
-  -> Flag "" '["vscode"] "" "vscode todo style" Bool
-  -> Flag "" '["chart"] "FILE_PATH" "chart file path" (Maybe FilePath)
-  -> Cmd "compile command" ()
-compile iPath oPath isVsCode chartPath = do
-  let input = maybe (fmap pack getContents) (fmap pack . readFile) (get iPath)
-      output = maybe (putStrLn . unpack) ((. unpack) . writeFile) (get oPath)
+data Argument = Argument
+  { _input             :: Maybe InputPath,
+    _output            :: Maybe OutputPath,
+    _inputOutput       :: Maybe InputOutputPath,
+    _isVsCodeTodoStyle :: IsVsCodeTodoStyle,
+    _chartPath         :: Maybe ChartPath
+  }
+
+newtype InputPath = InputPath FilePath
+
+newtype OutputPath = OutputPath FilePath
+
+newtype InputOutputPath = InputOutputPath FilePath
+
+newtype IsVsCodeTodoStyle = IsVsCodeTodoStyle Bool
+
+newtype ChartPath = ChartPath FilePath
+
+compile ::
+  Flag "i" '["input"] "FILE_PATH" "input file path" (Maybe FilePath) ->
+  Flag "o" '["output"] "FILE_PATH" "output file path" (Maybe FilePath) ->
+  Flag "" '["vscode"] "" "vscode todo style" Bool ->
+  Flag "" '["chart"] "FILE_PATH" "chart file path" (Maybe FilePath) ->
+  Arg "input-output" (Maybe FilePath) ->
+  Cmd "compile command" ()
+compile iPath oPath isVsCode chartPath ioPath = liftIO $ compile' $ Argument (InputPath <$> get iPath) (OutputPath <$> get oPath) (InputOutputPath <$> get ioPath) (IsVsCodeTodoStyle $ get isVsCode) (ChartPath <$> get chartPath)
+
+compile' :: Argument -> IO ()
+compile' Argument {..} = do
+  let input = maybe (fmap pack getContents) (fmap pack . readFile) (coerce _inputOutput <|> coerce _input)
+      output = maybe (putStrLn . unpack) ((. unpack) . writeFile) (coerce _inputOutput <|> coerce _output)
       outputForWork apms = fromMaybe (pure ()) $ do
-        path <- get oPath
+        path <- coerce _inputOutput <|> coerce _output
         forWorkPath <- changeFilePathForWork path
         pure $ writeFile forWorkPath . unpack . pprint $ ForWorkAPM <$> apms
-      chart = maybe (const $ pure ()) getChart (get chartPath)
-      wrapTodo | get isVsCode = Right . VsCodeTodo
-               | otherwise = Left
-  i <- liftIO input
-  liftIO $ case parse (some $ apmParser (treeParser (todoParser qwaParser)) <* optional newline) "" i of
+      chart = maybe (const $ pure ()) getChart (coerce _chartPath)
+      wrapTodo
+        | coerce _isVsCodeTodoStyle = Right . VsCodeTodo
+        | otherwise = Left
+  i <- input
+  case parse (some $ apmParser (treeParser (todoParser qwaParser)) <* optional newline) "" i of
     Left err -> putStrLn $ errorBundlePretty err
     Right as -> do
       let apms = (qwas %~ sortTodoQWA) <$> L.sort as
       output . pprint $ MDAPM . fmap (fmap wrapTodo) <$> apms
       outputForWork apms
       chart apms
-
 
 sortTodoQWA :: (Ord t, IsTodo t) => Vector (Tree t) -> Vector (Tree t)
 sortTodoQWA =
