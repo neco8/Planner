0f2b04880dddaef057b206b67041957198c1e692
neco8 <shogibeshow@gmail.com>
2022-02-01 02:49:40 +0000

    fix additional information pprint instance

--- haskell-abyss/src/AdditionalInformation.hs
+++ haskell-abyss/src/AdditionalInformation.hs
@@ -3,16 +3,19 @@ module AdditionalInformation where
 
 import           Control.Applicative  (Alternative (many))
 import           Data.Coerce          (coerce)
-import           Data.Text            (Text)
+import           Data.Text            (Text, null)
 import           PPrint               (PPrint (pprint))
 import           Parser               (Parser)
+import           Prelude              hiding (null)
 import           Text.Megaparsec      (MonadParsec (takeWhile1P))
 import           Text.Megaparsec.Char (string)
 
 newtype AdditionalInformation = AdditionalInformation Text deriving (Eq, Ord, Show)
 
 instance PPrint AdditionalInformation where
-  pprint a = " #" <> runAdditionalInformation a
+  pprint a
+    | not (null $ runAdditionalInformation a) = " #" <> runAdditionalInformation a
+    | otherwise = ""
 
 runAdditionalInformation :: AdditionalInformation -> Text
 runAdditionalInformation = coerce
