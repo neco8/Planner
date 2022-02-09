2a51464a3bcd50f65bc541dd62956054dd8ab402
neco8 <shogibeshow@gmail.com>
2022-02-09 21:57:18 +0900

    can now choice editor with editorPipe

--- haskell-abyss/src/EditorPipe.hs
+++ haskell-abyss/src/EditorPipe.hs
@@ -1,16 +1,18 @@
 module EditorPipe where
 
+import           Data.Maybe     (fromMaybe)
 import           Safe           (headMay)
 import           System.IO      (hClose, openTempFile)
 import           System.Process (createProcess, proc, waitForProcess)
 
 
-editorPipe :: String -> IO String
-editorPipe before = do
+editorPipe :: Maybe String -> String -> IO String
+editorPipe meditor before = do
   (path, handle) <- openTempFile "/tmp" "pipe.tmp"
   hClose handle
   writeFile path before
-  (_, _, _, processHandle) <- createProcess $ proc "vim" [path]
+  let editor = fromMaybe "vim" meditor
+  (_, _, _, processHandle) <- createProcess $ proc editor [path]
   waitForProcess processHandle
   reverse . dropWhen (== '\n') . reverse <$> readFile path
     where
